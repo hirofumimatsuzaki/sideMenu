d32c63245738405e0ea2e3bd42f55fb2a8d70c9a
hirofumimatsuzaki <hirofumimatsuzaki@gmail.com>
2020-07-10 20:12:25 +0900

    add task

--- src/app/pageone/pageone.page.ts
+++ src/app/pageone/pageone.page.ts
@@ -6,13 +6,23 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./pageone.page.scss'],
 })
 export class PageonePage implements OnInit {
+    task:string;
+    tasks:{ name:string }[]=[
 
+    ];
 
   constructor() { }
+ 
 
   ngOnInit() {
     
   }
 
+  addTask(){
+    this.tasks.push({
+      name:this.task
+    });
+    this.task='';
+  }
 
 }
