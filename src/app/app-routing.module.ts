ec3b3c175cb5c807a8fa3418ac66df2b03ef8a52
hirofumimatsuzaki <hirofumimatsuzaki@gmail.com>
2020-07-10 17:17:03 +0900

    Ionic project with ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'pagetwo',
+    loadChildren: () => import('./pagetwo/pagetwo.module').then( m => m.PagetwoPageModule)
   }
 ];
 
---
+++ src/app/pagetwo/pagetwo.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagetwo',
+  templateUrl: './pagetwo.page.html',
+  styleUrls: ['./pagetwo.page.scss'],
+})
+export class PagetwoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
